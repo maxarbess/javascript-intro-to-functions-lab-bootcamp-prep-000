3bc7f3a7975a981b2e2b482d5af956d62d9e86bc
Max Arbess <maxarbess1@gmail.com>
2019-01-14 04:26:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,6 @@ function sayHiToGrandma(string)  {
   return "I can't hear you!"
   if (string.toUpperCase()===string)
   return "YES INDEED!"
-  if (string===)
+  if (string==="I love you, Grandma.")
+  return "I love you, too"
 }
